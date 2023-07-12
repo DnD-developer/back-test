7aa0761b383030c3680525fa475ceea05e33f43e
DND-dev <dev.work@dnd-projects.ru>
2023-07-12 14:46:06 +0300

    back for ra16-hooks-context

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use(
-  bodyParser.json({
-    type(req) {
-      return true;
-    },
-  })
-);
+	bodyParser.json({
+		type(req) {
+			return true
+		}
+	})
+)
 app.use(function (req, res, next) {
-  res.setHeader('Content-Type', 'application/json');
-  next();
-});
+	res.setHeader("Content-Type", "application/json")
+	next()
+})
 
-const notes = [];
-let nextId = 1;
+app.get("/data", async (req, res) => {
+	res.send(JSON.stringify({ status: "ok" }))
+})
+app.get("/error", async (req, res) => {
+	res.status(500).send(JSON.stringify({ status: "Internal Error" }))
+})
+app.get("/loading", async (req, res) => {
+	await new Promise(resolve => {
+		setTimeout(() => {
+			resolve()
+		}, 5000)
+	})
+	res.send(JSON.stringify({ status: "ok" }))
+})
 
-app.get("/notes", (req, res) => {
-  res.send(JSON.stringify(notes));
-});
-
-app.post("/notes", (req, res) => {
-  notes.push({ ...req.body, id: nextId++ });
-  res.status(204);
-  res.end();
-});
-
-app.delete("/notes/:id", (req, res) => {
-  const noteId = Number(req.params.id);
-  const index = notes.findIndex((o) => o.id === noteId);
-  if (index !== -1) {
-    notes.splice(index, 1);
-  }
-  res.status(204);
-  res.end();
-});
-
-const port = process.env.PORT || 7070;
-app.listen(port, () => console.log(`The server is running on http://localhost:${port}`));
+const port = process.env.PORT || 7070
+app.listen(port, () => console.log(`The server is running on port ${port}.`))
+;``
