e808c446f9a1bfa618755a5cd0e7e35db853fb89
DND-dev <dev.work@dnd-projects.ru>
2023-07-30 17:12:24 +0300

    back for routers

--- server.js
+++ server.js
@@ -17,21 +17,49 @@ app.use(function (req, res, next) {
 	next()
 })
 
-app.get("/data", async (req, res) => {
-	res.send(JSON.stringify({ status: "ok" }))
+let posts = []
+let nextId = 1
+
+app.get("/posts", (req, res) => {
+	res.send(JSON.stringify(posts))
 })
-app.get("/error", async (req, res) => {
-	res.status(500).send(JSON.stringify({ status: "Internal Error" }))
+
+app.get("/posts/:id", (req, res) => {
+	const postId = Number(req.params.id)
+	const index = posts.findIndex(o => o.id === postId)
+	res.send(JSON.stringify({ post: posts[index] }))
 })
-app.get("/loading", async (req, res) => {
-	await new Promise(resolve => {
-		setTimeout(() => {
-			resolve()
-		}, 5000)
+
+app.post("/posts", (req, res) => {
+	posts.push({ ...req.body, id: nextId++, created: Date.now() })
+	res.status(204)
+	res.end()
+})
+
+app.put("/posts/:id", (req, res) => {
+	const postId = Number(req.params.id)
+	posts = posts.map(o => {
+		if (o.id === postId) {
+			return {
+				...o,
+				...req.body,
+				id: o.id
+			}
+		}
+		return o
 	})
-	res.send(JSON.stringify({ status: "ok" }))
+	res.status(204).end()
+})
+
+app.delete("/posts/:id", (req, res) => {
+	const postId = Number(req.params.id)
+	const index = posts.findIndex(o => o.id === postId)
+	if (index !== -1) {
+		posts.splice(index, 1)
+	}
+	res.status(204)
+	res.end()
 })
 
 const port = process.env.PORT || 7070
-app.listen(port, () => console.log(`The server is running on port ${port}.`))
-;``
+app.listen(port, () => console.log(`The server is running on ${port}`))
